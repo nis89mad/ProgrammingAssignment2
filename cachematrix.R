2c79509762163d037fae237ba17e94c54f604c41
nis89mad <nishan.madawanarachchi@gmail.com>
2016-03-03 22:31:59 +0530

    this is the changes done fill the necessary functions in the R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions will be used caching an inverse of matrix
 
-## Write a short comment describing this function
+## folowing function will be used generate a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- matrix()
+    get_matrix <- function() x
+    get_inv_matrix <- function() inv_mat
+    set_inv_matrix <- function(in_ma) inv_mat <<- in_ma
+    list(get_matrix = get_matrix,get_inv_matrix = get_inv_matrix, set_inv_matrix = set_inv_matrix)
+    
 }
 
 
-## Write a short comment describing this function
+## Following function will be used to calaculte the inverse of the matrix or to retrive the inverse
+## of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv_mat <- x$get_inv_matrix() 
+    if (!is.na(inv_mat[1,1])) {
+        message("Taking the value from cache")
+        return (inv_mat)
+    }
+    matrixx <- x$get_matrix()
+    inv_mat <- solve(matrixx)
+    x$set_inv_matrix(inv_mat)
+    inv_mat
 }
