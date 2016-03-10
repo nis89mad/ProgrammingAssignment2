87fcdc1bc830a07f62b977aa887bfd4012636175
nis89mad <nishan.madawanarachchi@gmail.com>
2016-03-10 06:41:47 +0530

    Some modifications to function validate inv_mat using is.null rather than is.na. initially inv_mat was set as empty matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,27 @@
-## these functions will be used caching an inverse of matrix
+## these functions will be used in caching an inverse of matrix
 
 ## folowing function will be used generate a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv_mat <- matrix()
+    inv_mat <- NULL
+    set_matrix <-  function(y) {
+        x <<- y
+        inv_mat <<- NULL
+    }
     get_matrix <- function() x
     get_inv_matrix <- function() inv_mat
     set_inv_matrix <- function(in_ma) inv_mat <<- in_ma
-    list(get_matrix = get_matrix,get_inv_matrix = get_inv_matrix, set_inv_matrix = set_inv_matrix)
-    
+    list(set_matrix = set_matrix,get_matrix = get_matrix,get_inv_matrix = get_inv_matrix, set_inv_matrix = set_inv_matrix)
 }
 
 
 ## Following function will be used to calaculte the inverse of the matrix or to retrive the inverse
-## of a matrix
+## if it is alredy cached
 
 cacheSolve <- function(x, ...) {
     inv_mat <- x$get_inv_matrix() 
-    if (!is.na(inv_mat[1,1])) {
-        message("Taking the value from cache")
+    if (!is.null(inv_mat)) {
+        message("Getting cached data")
         return (inv_mat)
     }
     matrixx <- x$get_matrix()
